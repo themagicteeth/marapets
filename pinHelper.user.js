2c97a4c31df4238be22bfee2b953e187fd2f8c4f
Will Schulzetenberg <wschulzetenberg@wddco.com>
2022-06-30 11:15:25 -0500

    PIN Helper

---
+++ pinHelper.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        PIN Helper
+// @namespace   Marascripts
+// @description Automatically fills in your pin at the ATM.
+// @grant       none
+// @author      themagicteeth
+// @version     1.0.0
+// @match       https://www.marapets.com/atm.php
+// @license     MIT
+// ==/UserScript==
+
+/**
+ * Put PIN here.
+ */
+const MY_PIN = "0000"
+
+// Deposit and Withdraw input boxes
+const DEPOSIT = document.querySelector(
+  "form[action='https://www.marapets.com/atm.php?do=deposit'] input[name='pin']"
+)
+const WITHDRAW = document.querySelector(
+  "form[action='https://www.marapets.com/atm.php?do=withdraw'] input[name='pin']"
+)
+
+// Set the value to your PIN
+DEPOSIT.value = MY_PIN
+WITHDRAW.value = MY_PIN
