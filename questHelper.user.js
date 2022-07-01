5c764dc764160c1c273491514f083921d501f171
Will Schulzetenberg <wschulzetenberg@wddco.com>
2022-07-01 14:38:44 -0500

    Quest helper

---
+++ questHelper.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        Quest Helper
+// @namespace   Marascripts
+// @description Automates questing.
+// @updateURL   https://github.com/themagicteeth/marapets/raw/main/
+// @grant       none
+// @author      themagicteeth
+// @version     1.0.0
+// @match       https://www.marapets.com/brew.php*
+// @match       https://www.marapets.com/candytree.php*
+// @match       https://www.marapets.com/farm.php*
+// @match       https://www.marapets.com/garage.php*
+// @match       https://www.marapets.com/computer.php*
+// @match       https://www.marapets.com/carpenter.php*
+// @match       https://www.marapets.com/elger.php*
+// @match       https://www.marapets.com/traveller.php*
+// @match       https://www.marapets.com/leprechaun.php*
+// @match       https://www.marapets.com/inn.php*
+// @match       https://www.marapets.com/bee.php*
+// @match       https://www.marapets.com/monster.php*
+// @match       https://www.marapets.com/excavator.php*
+// @match       https://www.marapets.com/explorer.php*
+// @match       https://www.marapets.com/truck.php*
+// @match       https://www.marapets.com/viewstock.php*
+// @match       https://www.marapets.com/shop.php*
+// @match       https://www.marapets.com/shops.php*
+// @homepageURL https://github.com/themagicteeth/marapets
+// @supportURL  https://github.com/themagicteeth/marapets/issues
+// @icon        https://marapets.com/favicon.ico
+// @license     MIT
+// ==/UserScript==
+/*jshint -W033 */
+
+"use strict"
