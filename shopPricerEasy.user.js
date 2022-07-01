78b1979f7039734976d7e76534c24d2f7b0d585c
Will Schulzetenberg <wschulzetenberg@wddco.com>
2022-07-01 14:20:46 -0500

    Formatting

--- shopPricerEasy.user.js
+++ shopPricerEasy.user.js
@@ -22,31 +22,20 @@ if (document.referrer.includes("/viewstock")) {
    * */
   const TIMEOUT = 40000
 
-  const noReprice = document.querySelector(
-    ".maralayoutmiddle .bigger.banned.middleit"
-  ) // Nothing to reprice
-  const changed = document
-    .querySelector(".maralayoutmiddle .bigger.middleit")
-    .innerText.search("You have changed") // Confirmed reprices
-  const autoPriced = document
-    .querySelector(".maralayoutmiddle .bigger.middleit")
-    .innerText.search("has changed") // Repricer changed prices
+  const noReprice = document.querySelector(".maralayoutmiddle .bigger.banned.middleit") // Nothing to reprice
+  const changed = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search("You have changed") // Confirmed reprices
+  const autoPriced = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search("has changed") // Repricer changed prices
 
   // Click the "Auto Price" button, if none of the messages are present
-  if (autoPriced === -1 && changed === -1 && !noReprice) {
-    document.querySelector("input[value='Auto Price']").click()
-  }
-
+  if (autoPriced === -1 && changed === -1 && !noReprice) document.querySelector("input[value='Auto Price']").click()
+  
   // Click the "Update Prices" button, if the auto pricer message is there
   if (autoPriced !== -1) {
     // const mediumDrop = document.querySelectorAll(".marapets_border14")
     const largeDrop = document.querySelectorAll(".marapets_border15")
     if (largeDrop.length !== 0) {
       largeDrop.forEach((item) => {
-        const oldPrice = item
-          .querySelector(".mp")
-          .innerText.split("MP")[0]
-          .replace(/,/g, "")
+        const oldPrice = item.querySelector(".mp").innerText.split("MP")[0].replace(/,/g, "")
         item.querySelector("input").value = oldPrice
       })
     }
@@ -56,9 +45,7 @@ if (document.referrer.includes("/viewstock")) {
 
   // Go to the next page, or alert that we are finished
   if (noReprice || changed !== -1) {
-    const pageButtons = document
-      .querySelectorAll(".pages_all")[0]
-      .querySelectorAll(".pages_each a") // Array of the page selector
+    const pageButtons = document.querySelectorAll(".pages_all")[0].querySelectorAll(".pages_each a") // Array of the page selector
     const secondToLastPageButton = pageButtons[pageButtons.length - 2] // The second to last button
 
     // If the second to last button says "Next >", click it otherwise notify we are finished
